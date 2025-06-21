5981ec180e27cd56e1dd198fe0099d9a3339a3ca
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-06-21 08:49:57 +0800

    style: add navbar with gradient bg

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <h1 className='navbar__logo'>CV Maker</h1>
+    </nav>
+  )
+}
+
+export default Navbar
