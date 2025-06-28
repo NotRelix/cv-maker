48243296f95387d2b7334ccfbebc87fd8709d995
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-06-27 21:04:43 -0400

    feat: add backdropto cv on smaller screens

--- src/components/UserCV.jsx
+++ src/components/UserCV.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from 'react';
 import '../styles/UserCV.css';
 import BasicInfo from './BasicInfo';
 import ContactInfo from './ContactInfo';
 
 const UserCV = ({ userInfo, toggleCV }) => {
+  const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 1000px)').matches);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 1000px)');
+
+    const handleMediaChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaChange);
+
+    setIsMobile(mediaQuery.matches);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaChange);
+    };
+  }, []);
+
   return (
-    <section className={`user-cv__container ${toggleCV === false ? "user-cv-hidden" : ""}`}>
-      <section className="user-cv__top-section">
-        <BasicInfo userInfo={userInfo} />
-        <ContactInfo userInfo={userInfo} />
+    <>
+      {toggleCV && isMobile && <div className='user-cv__backdrop' />}
+
+      <section className={`user-cv__container ${toggleCV === false ? "user-cv-hidden" : ""}`}>
+        <section className="user-cv__top-section">
+          <BasicInfo userInfo={userInfo} />
+          <ContactInfo userInfo={userInfo} />
+        </section>
       </section>
-    </section>
+    </>
   )
 }
 
